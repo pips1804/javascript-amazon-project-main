86c796cd15aeaa61451b59a650362b3774ac35ff
JPManlapaz <jhonpaulmanlapaz1804@gmail.com>
2024-07-21 21:30:29 +0800

    Save cart in localstorage

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,17 @@
-export let cart = [
-  {
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2,
-  },
-  {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1,
-  },
-];
+export let cart = JSON.parse(localStorage.getItem("cart"));
+
+if (!cart) {
+  cart = [
+    {
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 2,
+    },
+    {
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity: 1,
+    },
+  ];
+}
 
 export function addToCart(productId) {
   let matchingItem;
@@ -31,9 +35,15 @@ export function addToCart(productId) {
       quantity,
     });
   }
+
+  saveToStorage();
 }
 
-//
+function saveToStorage() {
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
+
+//Remove the product from the cart
 export function removerFromCart(productId) {
   const newCart = [];
 
@@ -44,4 +54,6 @@ export function removerFromCart(productId) {
   });
 
   cart = newCart;
+
+  saveToStorage();
 }
