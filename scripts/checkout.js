fc5800cd60ae6857d7fb46d7735fbff15f150885
JPManlapaz <jhonpaulmanlapaz1804@gmail.com>
2024-07-25 20:14:11 +0800

    create oop version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
+import "../data/cart-oop.js";
 // This uses a module that has a function so when we make changes to our cart it will automatically update and load the HTML
 renderCheckoutHeader();
 renderOrderSummary();
